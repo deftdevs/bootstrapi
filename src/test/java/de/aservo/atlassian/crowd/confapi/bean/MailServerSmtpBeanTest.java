b20863ac5049d624de88304ae9062c6386c8090d
Martin Grambauer <martin@just-qa.de>
2020-03-06 07:01:34 +0100

    added implementations to get and set the basic mail configuration in crowd

---
+++ src/test/java/de/aservo/atlassian/crowd/confapi/bean/MailServerSmtpBeanTest.java
@@ -0,0 +1,92 @@
+package de.aservo.atlassian.crowd.confapi.bean;
+
+import com.atlassian.crowd.manager.mail.MailConfiguration;
+import com.atlassian.crowd.util.mail.SMTPServer;
+import de.aservo.atlassian.confapi.model.MailServerSmtpBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class MailServerSmtpBeanTest {
+
+    private MailConfiguration getDefaultMailConfiguration() throws AddressException {
+        return MailConfiguration.builder()
+                .setServerAlertAddress("alert@aservo.com")
+                .setSmtpServer(SMTPServer.builder()
+                        .setPrefix("[ASERVO]")
+                        .setFrom(new InternetAddress("mail@aservo.com"))
+                        .setHost("host.aservo.com")
+                        .build())
+                .build();
+    }
+
+    @Test
+    public void testDefaultConstructor() {
+        final MailServerSmtpBean bean = new MailServerSmtpBean();
+
+        assertNull(bean.getAdminContact());
+        assertNull(bean.getFrom());
+        assertNull(bean.getPrefix());
+        assertNull(bean.getHost());
+    }
+
+    @Test
+    public void testConstructor() throws AddressException {
+        final MailServerSmtpBean bean = new MailServerSmtpBean(
+                getDefaultMailConfiguration().getServerAlertAddress(),
+                getDefaultMailConfiguration().getSmtpServer().getFrom().toString(),
+                getDefaultMailConfiguration().getSmtpServer().getPrefix(),
+                getDefaultMailConfiguration().getSmtpServer().getHost()
+        );
+        assertEquals(bean.getAdminContact(), getDefaultMailConfiguration().getServerAlertAddress());
+        assertEquals(bean.getFrom(), getDefaultMailConfiguration().getSmtpServer().getFrom().toString());
+        assertEquals(bean.getPrefix(), getDefaultMailConfiguration().getSmtpServer().getPrefix());
+        assertEquals(bean.getHost(), getDefaultMailConfiguration().getSmtpServer().getHost());
+    }
+
+    @Test
+    public void testEqualsAndHashCode() throws Exception {
+        final MailServerSmtpBean bean1 = new MailServerSmtpBean(
+                getDefaultMailConfiguration().getServerAlertAddress(),
+                getDefaultMailConfiguration().getSmtpServer().getFrom().toString(),
+                getDefaultMailConfiguration().getSmtpServer().getPrefix(),
+                getDefaultMailConfiguration().getSmtpServer().getHost()
+        );
+
+        final MailServerSmtpBean bean2 = new MailServerSmtpBean(
+                getDefaultMailConfiguration().getServerAlertAddress(),
+                getDefaultMailConfiguration().getSmtpServer().getFrom().toString(),
+                getDefaultMailConfiguration().getSmtpServer().getPrefix(),
+                getDefaultMailConfiguration().getSmtpServer().getHost()
+        );
+
+        assertEquals(bean1, bean2);
+        assertEquals(bean1.hashCode(), bean2.hashCode());
+    }
+
+    @Test
+    public void testEqualsSameInstance() {
+        final MailServerSmtpBean bean = new MailServerSmtpBean();
+        assertEquals(bean, bean);
+    }
+
+    @Test
+    public void testEqualsNull() {
+        final MailServerSmtpBean bean = new MailServerSmtpBean();
+        assertNotEquals(null, bean);
+    }
+
+    @Test
+    public void testEqualsOtherType() {
+        final MailServerSmtpBean bean = new MailServerSmtpBean();
+        assertNotEquals(bean, new Object());
+    }
+
+}
