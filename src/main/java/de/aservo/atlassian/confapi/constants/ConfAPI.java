62376ff283f12b1558d34ef6ee0e226e47618cb8
Kai Lehmann <kai.uni-assist@lekatech.de>
2020-03-26 13:36:43 +0100

    additional milestone 1 REST resource names collection
    this commit contains all required additional resource path constants required for milestone
    https://github.com/aservo/confluence-confapi-plugin/milestone/1
    
    proposal examples:
    - License: {{ confluenceUrl  }}/{{ confluenceDomain  }}/rest/confapi/1/license
    - Anonymous Access configuration: {{ confluenceUrl  }}/{{ confluenceDomain  }}/rest/confapi/1/permissions/anonymous-access
    - User email address & password: {{ confluenceUrl  }}/{{ confluenceDomain  }}/rest/confapi/1/users
    - External gadgets: {{ confluenceUrl  }}/{{ confluenceDomain  }}/rest/confapi/1/gadgets/external
    - Application links (to e.g. Jira): {{ confluenceUrl  }}/{{ confluenceDomain  }}/rest/confapi/1/application-links

--- src/main/java/de/aservo/atlassian/confapi/constants/ConfAPI.java
+++ src/main/java/de/aservo/atlassian/confapi/constants/ConfAPI.java
@@ -2,14 +2,22 @@ package de.aservo.atlassian.confapi.constants;
 
 public class ConfAPI {
 
-    private ConfAPI() {}
+    public static final String APPLICATION_LINKS            = "application-links";
+    public static final String DIRECTORIES                  = "directories";
+    public static final String DIRECTORY                    = "directory";
+    public static final String GADGET_EXTERNAL              = "external";
+    public static final String GADGETS                      = "gadgets";
+    public static final String LICENSES                     = "licenses";
+    public static final String MAIL                         = "mail";
+    public static final String MAIL_POP                     = "pop";
+    public static final String MAIL_SMTP                    = "smtp";
+    public static final String PERMISSION_ANONYMOUS_ACCESS  = "anonymous-access";
+    public static final String PERMISSIONS                  = "permissions";
+    public static final String PING                         = "ping";
+    public static final String SETTINGS                     = "settings";
+    public static final String USERS                        = "users";
 
-    public static final String DIRECTORIES = "directories";
-    public static final String DIRECTORY   = "directory";
-    public static final String MAIL        = "mail";
-    public static final String MAIL_POP    = "pop";
-    public static final String MAIL_SMTP   = "smtp";
-    public static final String PING        = "ping";
-    public static final String SETTINGS    = "settings";
+    private ConfAPI() {
+    }
 
 }
