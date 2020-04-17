81a76e8d8b4d6dc79457c731ab0cc41d9142469d
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-04-17 16:05:26 +0800

    Add mode to SettingsBean, needed in Jira

--- src/test/java/de/aservo/atlassian/confapi/model/SettingsBeanTest.java
+++ src/test/java/de/aservo/atlassian/confapi/model/SettingsBeanTest.java
@@ -17,20 +17,24 @@ public class SettingsBeanTest {
         final SettingsBean bean = new SettingsBean();
 
         assertNull(bean.getBaseUrl());
+        assertNull(bean.getMode());
         assertNull(bean.getTitle());
     }
 
     @Test
     public void testParameterConstructor() throws Exception {
         final String baseUrl = "http://localhost/product";
+        final String mode = "PRIVATE";
         final String title = "PRODUCT";
 
         final SMTPMailServer server = new DefaultTestSmtpMailServerImpl();
         final SettingsBean bean = new SettingsBean();
         bean.setBaseUrl(baseUrl);
+        bean.setMode(mode);
         bean.setTitle(title);
 
         assertEquals(baseUrl, bean.getBaseUrl());
+        assertEquals(mode.toLowerCase(), bean.getMode());
         assertEquals(title, bean.getTitle());
     }
 
