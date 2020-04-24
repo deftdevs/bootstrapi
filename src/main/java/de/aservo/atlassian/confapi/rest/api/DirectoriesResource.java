41acfa76b62dd2614e8ad6d4e57eadec9964fa2e
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-04-24 11:54:49 +0800

    Move resource implementation to ConfAPI Commons
    During the process of standardizing the ConfAPI plugins, more and more of the
    REST and the underlying service interfaces have moved to the Commons library.
    This lead to the current state that the actual (default) implementation of the
    resources also can be moved to the Commons library, while the actual service
    implementations should be moved (back) to the actual plugins in order to get rid
    of fixed Atlassian dependencies in the Commons library (see #13).
    
    This commit contains two PoC's for REST resources that are now implemented in
    the Commons library. First the PingResource that does not contain any services
    at all and second the DirectoriesResource that uses a custom service only.
    
    While the DirectoryService interface will stay in the Commons library, the
    DirectoryServiceImpl will be moved to the actual plugins after.
    
    In order to use one of the here implemented in an actual plugin, the developer
    only needs to extend from the Abstract<Entity>ResourceImpl, inject the
    service(s) and add the corresponding `@Path` to the class.
    
    Snapshot version bump because of interface change in DirectoriesResource.

--- src/main/java/de/aservo/atlassian/confapi/rest/api/DirectoriesResource.java
+++ src/main/java/de/aservo/atlassian/confapi/rest/api/DirectoriesResource.java
@@ -10,13 +10,19 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public interface DirectoriesResource {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Operation(
             tags = { ConfAPI.DIRECTORIES },
             summary = "Get the list of user directories",
@@ -28,17 +34,19 @@ public interface DirectoriesResource {
     Response getDirectories();
 
     @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Operation(
             tags = { ConfAPI.DIRECTORIES },
-            summary = "Add a new directory",
+            summary = "Set a new directory",
             description = "Any existing directory with the same name will be removed before adding the new one",
             responses = {
                     @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = DirectoryBean.class))),
                     @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorCollection.class)))
             }
     )
-    Response addDirectory(
-            boolean testConnection,
+    Response setDirectory(
+            @QueryParam("test-connection") @DefaultValue("false") final boolean testConnection,
             @NotNull final DirectoryBean directory);
 
 }
