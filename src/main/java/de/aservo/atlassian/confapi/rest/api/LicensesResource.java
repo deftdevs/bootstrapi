1ab0bdf5e690368f641f0a4a21a70779d278826e
Kai Lehmann <kai.lehmann@lekatech.de>
2020-05-19 11:20:15 +0200

    License resource with addLicense method
    Due to a resource addressing conflict (path + mediatype) the
    license resource was redesigned. Now, there is a dedicated
    POST addLicense resource which adds a new license to an already
    existing license collection.
    
    The PUT setLicenses resource replaces the existing license
    collection.
    
    Because of this redesign the 'clear' flag was removed. Clearing
    license details is achieved with the setLicenses method.

--- src/main/java/de/aservo/atlassian/confapi/rest/api/LicensesResource.java
+++ src/main/java/de/aservo/atlassian/confapi/rest/api/LicensesResource.java
@@ -5,18 +5,16 @@ import de.aservo.atlassian.confapi.model.ErrorCollection;
 import de.aservo.atlassian.confapi.model.LicenseBean;
 import de.aservo.atlassian.confapi.model.LicensesBean;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -41,30 +39,27 @@ public interface LicensesResource {
     @Operation(
             tags = { ConfAPI.LICENSES },
             summary = "Set a new set of license",
-            description = "Existing license details are overwritten. Upon successful request, returns a `LicensesBean` object containing license details",
+            description = "Existing license details are always cleared before setting the new licenses. Upon successful request, returns a `LicensesBean` object containing license details",
             responses = {
                     @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = LicensesBean.class))),
                     @ApiResponse(responseCode = "default", content = @Content(schema = @Schema(implementation = ErrorCollection.class))),
             }
     )
     Response setLicenses(
-            @Parameter(description="Clear license details before updating (Jira only).") @QueryParam ("clear") @DefaultValue("false") final boolean clear,
             @NotNull final LicensesBean licensesBean);
 
-    @PUT
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(
             tags = { ConfAPI.LICENSES },
-            summary = "Set a new license",
-            description = "Existing license details are overwritten. Upon successful request, returns a `LicensesBean` object containing license details",
+            summary = "Adds a single license",
+            description = "Upon successful request, returns a `LicensesBean` object containing license details",
             responses = {
                     @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = LicensesBean.class))),
                     @ApiResponse(responseCode = "default", content = @Content(schema = @Schema(implementation = ErrorCollection.class))),
             }
     )
-    Response setLicense(
-            @Parameter(description="Clear license details before updating (Jira only).") @QueryParam ("clear") @DefaultValue("false") final boolean clear,
+    Response addLicense(
             @NotNull final LicenseBean licenseBean);
-
 }
