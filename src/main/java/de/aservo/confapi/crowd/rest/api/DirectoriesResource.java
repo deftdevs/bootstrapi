036f4c02ec1e64ff600fad353213ce91e2e5fea4
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-06-17 19:27:59 +0800

    Generate documentation with Swagger

--- src/main/java/de/aservo/confapi/crowd/rest/api/DirectoriesResource.java
+++ src/main/java/de/aservo/confapi/crowd/rest/api/DirectoriesResource.java
@@ -1,5 +1,14 @@
 package de.aservo.confapi.crowd.rest.api;
 
+import de.aservo.confapi.commons.constants.ConfAPI;
+import de.aservo.confapi.commons.model.ErrorCollection;
+import de.aservo.confapi.crowd.model.DirectoryBean;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,7 +20,16 @@ public interface DirectoriesResource {
 
     @GET
     @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            tags = { ConfAPI.DIRECTORIES },
+            summary = "Get a directory based on it's ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = DirectoryBean.class))),
+                    @ApiResponse(responseCode = "default", content = @Content(schema = @Schema(implementation = ErrorCollection.class)))
+            }
+    )
     Response getDirectory(
             @PathParam("id") final long id);
 
