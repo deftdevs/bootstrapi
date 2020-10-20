27c4378a3010198e0eb493946071f67c603383ea
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-10-20 10:46:00 +0800

    Improve mail server bean methods
    - Allow setting empty description now
    - Allow setting port from int and String
    - Provide constant for default timeout

--- src/test/java/de/aservo/confapi/commons/model/MailServerSmtpBeanTest.java
+++ src/test/java/de/aservo/confapi/commons/model/MailServerSmtpBeanTest.java
@@ -1,26 +1,10 @@
 package de.aservo.confapi.commons.model;
 
 import de.aservo.confapi.commons.junit.AbstractBeanTest;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
 @RunWith(MockitoJUnitRunner.class)
 public class MailServerSmtpBeanTest extends AbstractBeanTest {
-
-    @Test
-    public void testTimeoutHasDefaultValue() {
-        final long timeout = 12345L;
-        final MailServerSmtpBean beanWithTimeoutSet = new MailServerSmtpBean();
-
-        beanWithTimeoutSet.setTimeout(timeout);
-        assertEquals(timeout, beanWithTimeoutSet.getTimeout().longValue());
-
-        final MailServerSmtpBean beanWithoutTimeoutSet = new MailServerSmtpBean();
-        assertNotNull(beanWithoutTimeoutSet.getTimeout());
-    }
 
 }
