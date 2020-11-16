5ac6e06ac67d623ac301b159a2c091ef86c9bb8e
Kai Lehmann <kai.lehmann@lekatech.de>
2020-11-16 13:27:27 +0100

    Removal of ambigious codehaus/fasterxml annotations which failed openapi generation

--- src/main/java/de/aservo/confapi/commons/model/AbstractDirectoryBean.java
+++ src/main/java/de/aservo/confapi/commons/model/AbstractDirectoryBean.java
@@ -1,11 +1,10 @@
 package de.aservo.confapi.commons.model;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import de.aservo.confapi.commons.constants.ConfAPI;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlElement;
@@ -20,24 +19,14 @@ import java.util.Date;
 @XmlRootElement
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
-        property = "type"
-)
-@org.codehaus.jackson.annotate.JsonTypeInfo(
-        use = org.codehaus.jackson.annotate.JsonTypeInfo.Id.NAME,
-        include = org.codehaus.jackson.annotate.JsonTypeInfo.As.PROPERTY,
+        include = JsonTypeInfo.As.PROPERTY,
         property = "type"
 )
 @JsonSubTypes({
-        @Type(value = DirectoryCrowdBean.class, name = ConfAPI.DIRECTORY_CROWD),
-        @Type(value = DirectoryGenericBean.class, name = ConfAPI.DIRECTORY_GENERIC),
-        @Type(value = DirectoryInternalBean.class, name = ConfAPI.DIRECTORY_INTERNAL),
-        @Type(value = DirectoryLdapBean.class, name = ConfAPI.DIRECTORY_LDAP),
-})
-@org.codehaus.jackson.annotate.JsonSubTypes({
-        @org.codehaus.jackson.annotate.JsonSubTypes.Type(value = DirectoryCrowdBean.class, name = ConfAPI.DIRECTORY_CROWD),
-        @org.codehaus.jackson.annotate.JsonSubTypes.Type(value = DirectoryGenericBean.class, name = ConfAPI.DIRECTORY_GENERIC),
-        @org.codehaus.jackson.annotate.JsonSubTypes.Type(value = DirectoryInternalBean.class, name = ConfAPI.DIRECTORY_INTERNAL),
-        @org.codehaus.jackson.annotate.JsonSubTypes.Type(value = DirectoryLdapBean.class, name = ConfAPI.DIRECTORY_LDAP),
+        @JsonSubTypes.Type(value = DirectoryCrowdBean.class, name = ConfAPI.DIRECTORY_CROWD),
+        @JsonSubTypes.Type(value = DirectoryGenericBean.class, name = ConfAPI.DIRECTORY_GENERIC),
+        @JsonSubTypes.Type(value = DirectoryInternalBean.class, name = ConfAPI.DIRECTORY_INTERNAL),
+        @JsonSubTypes.Type(value = DirectoryLdapBean.class, name = ConfAPI.DIRECTORY_LDAP),
 })
 public abstract class AbstractDirectoryBean {
 
