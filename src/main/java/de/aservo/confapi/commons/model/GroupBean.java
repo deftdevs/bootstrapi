04a53c8708f53027fe89001cc55497ca95e9d813
Patrick Hobusch <patrick@hobusch.net>
2023-11-06 19:07:14 +0800

    Introduce groups, extend user bean by first name, last name and groups

---
+++ src/main/java/de/aservo/confapi/commons/model/GroupBean.java
@@ -0,0 +1,47 @@
+package de.aservo.confapi.commons.model;
+
+import de.aservo.confapi.commons.constants.ConfAPI;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Bean for groups REST requests.
+ */
+@Data
+@NoArgsConstructor
+@XmlRootElement(name = ConfAPI.GROUP)
+public class GroupBean {
+
+    @XmlElement
+    private String name;
+
+    @XmlElement
+    private String description;
+
+    @XmlElement
+    private Boolean active;
+
+    // Example instances for documentation and tests
+
+    public static final GroupBean EXAMPLE_1;
+
+    static {
+        EXAMPLE_1 = new GroupBean();
+        EXAMPLE_1.setName("example");
+        EXAMPLE_1.setDescription("Example Group");
+        EXAMPLE_1.setActive(true);
+    }
+
+    public static final GroupBean EXAMPLE_2;
+
+    static {
+        EXAMPLE_2 = new GroupBean();
+        EXAMPLE_2.setName("other");
+        EXAMPLE_2.setDescription("Other Group");
+        EXAMPLE_2.setActive(false);
+    }
+
+}
--- src/main/java/de/aservo/confapi/commons/model/UserBean.java
+++ src/main/java/de/aservo/confapi/commons/model/UserBean.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
 
 /**
  * Bean for users REST requests.
@@ -20,6 +21,12 @@ public class UserBean {
     private String username;
 
     @XmlElement
+    private String firstName;
+
+    @XmlElement
+    private String lastName;
+
+    @XmlElement
     private String fullName;
 
     @XmlElement
@@ -29,6 +36,9 @@ public class UserBean {
     @EqualsAndHashCode.Exclude
     private String password;
 
+    @XmlElement
+    private List<GroupBean> groups;
+
     // Example instances for documentation and tests
 
     public static final UserBean EXAMPLE_1;
@@ -36,6 +46,8 @@ public class UserBean {
     static {
         EXAMPLE_1 = new UserBean();
         EXAMPLE_1.setUsername("example");
+        EXAMPLE_1.setFirstName("Example");
+        EXAMPLE_1.setLastName("User");
         EXAMPLE_1.setFullName("Example User");
         EXAMPLE_1.setEmail("user@example.com");
         EXAMPLE_1.setPassword("3x4mpl3");
@@ -46,6 +58,8 @@ public class UserBean {
     static {
         EXAMPLE_2 = new UserBean();
         EXAMPLE_2.setUsername("example");
+        EXAMPLE_2.setFirstName("Changed");
+        EXAMPLE_2.setLastName("Example User");
         EXAMPLE_2.setFullName("Changed Example User");
         EXAMPLE_2.setEmail("user@new-example.com");
     }
@@ -54,6 +68,8 @@ public class UserBean {
 
     static {
         EXAMPLE_3_ADMIN = new UserBean();
+        EXAMPLE_3_ADMIN.setFirstName("Admini");
+        EXAMPLE_3_ADMIN.setLastName("Strator");
         EXAMPLE_3_ADMIN.setFullName("Administrator");
         EXAMPLE_3_ADMIN.setUsername("admin");
         EXAMPLE_3_ADMIN.setEmail("admin@admin.de");
