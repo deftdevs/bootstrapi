7cc5413982f4dd4d1fddf1e8996edb68950a17e4
Patrick Hobusch <patrick@hobusch.net>
2024-02-26 14:05:40 +0800

    Update JUnit and Mockito to versions 5.x

--- src/test/java/de/aservo/confapi/confluence/filter/SysAdminOnlyResourceFilterTest.java
+++ src/test/java/de/aservo/confapi/confluence/filter/SysAdminOnlyResourceFilterTest.java
@@ -15,7 +15,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.*;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
