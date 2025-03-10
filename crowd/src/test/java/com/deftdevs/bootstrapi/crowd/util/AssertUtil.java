9b1e277c19b2830a177fabbe0ee87815600bd906
Patrick Hobusch <patrick.hobusch@deftdevs.com>
2025-03-11 00:31:20 +0800

    Remove product-specific test dependencies

--- crowd/src/test/java/com/deftdevs/bootstrapi/crowd/util/AssertUtil.java
+++ crowd/src/test/java/com/deftdevs/bootstrapi/crowd/util/AssertUtil.java
@@ -1,14 +1,14 @@
 package com.deftdevs.bootstrapi.crowd.util;
 
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 public class AssertUtil {
 
     public static void assertEquals(Object first, Object second, boolean firstParameterIsExpected) {
         if (firstParameterIsExpected) {
-            Assert.assertEquals(first, second);
+            Assertions.assertEquals(first, second);
         } else {
-            Assert.assertEquals(second, first);
+            Assertions.assertEquals(second, first);
         }
     }
 
