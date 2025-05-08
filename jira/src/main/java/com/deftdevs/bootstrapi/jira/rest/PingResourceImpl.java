cc5a33e08f82bd236bc3725f7fd5bdaa736ef81f
Patrick Hobusch <patrick.hobusch@deftdevs.com>
2025-05-08 21:58:22 +0800

    WIP

--- jira/src/main/java/com/deftdevs/bootstrapi/jira/rest/PingResourceImpl.java
+++ jira/src/main/java/com/deftdevs/bootstrapi/jira/rest/PingResourceImpl.java
@@ -1,6 +1,6 @@
 package com.deftdevs.bootstrapi.jira.rest;
 
-import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
+import com.atlassian.plugins.rest.api.security.annotation.UnrestrictedAccess;
 import com.deftdevs.bootstrapi.commons.constants.BootstrAPI;
 import com.deftdevs.bootstrapi.commons.rest.AbstractPingResourceImpl;
 import org.springframework.stereotype.Component;
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import javax.ws.rs.Path;
 
 @Path(BootstrAPI.PING)
-@AnonymousAllowed
+@UnrestrictedAccess
 @Component
 public class PingResourceImpl extends AbstractPingResourceImpl {
 
