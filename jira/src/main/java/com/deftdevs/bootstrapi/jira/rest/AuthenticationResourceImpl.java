9de79526d6f936fcf8cafec7c14f6ed96bcabcb2
Patrick Hobusch <patrick.hobusch@deftdevs.com>
2025-05-08 21:58:22 +0800

    WIP

--- jira/src/main/java/com/deftdevs/bootstrapi/jira/rest/AuthenticationResourceImpl.java
+++ jira/src/main/java/com/deftdevs/bootstrapi/jira/rest/AuthenticationResourceImpl.java
@@ -1,19 +1,18 @@
 package com.deftdevs.bootstrapi.jira.rest;
 
+import com.atlassian.plugins.rest.api.security.annotation.SystemAdminOnly;
 import com.deftdevs.bootstrapi.commons.constants.BootstrAPI;
 import com.deftdevs.bootstrapi.commons.model.AbstractAuthenticationIdpBean;
 import com.deftdevs.bootstrapi.commons.model.AuthenticationSsoBean;
 import com.deftdevs.bootstrapi.commons.rest.AbstractAuthenticationResourceImpl;
-import com.deftdevs.bootstrapi.jira.filter.SysadminOnlyResourceFilter;
 import com.deftdevs.bootstrapi.jira.service.api.JiraAuthenticationService;
-import com.sun.jersey.spi.container.ResourceFilters;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import javax.ws.rs.Path;
 
 @Path(BootstrAPI.AUTHENTICATION)
-@ResourceFilters(SysadminOnlyResourceFilter.class)
+@SystemAdminOnly
 @Component
 public class AuthenticationResourceImpl extends AbstractAuthenticationResourceImpl<AbstractAuthenticationIdpBean, AuthenticationSsoBean, JiraAuthenticationService> {
 
